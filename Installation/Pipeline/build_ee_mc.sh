9db73bc0fa8d9d6cd607e19ab04b990b57358f73
Frank Celler <frank@arangodb.com>
2017-06-03 17:05:07 +0200

    added mac

---
+++ Installation/Pipeline/build_ee_mc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+concurrency=$1
+
+mkdir -p build-jenkins
+mkdir -p build/bin
+mkdir -p build/etc
+mkdir -p build/tests
+
+cd build-jenkins
+CXXFLAGS=-fno-omit-frame-pointer cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_MAINTAINER_MODE=On -DUSE_CATCH_TESTS=On -DUSE_FAILURE_TESTS=On -DDEBUG_SYNC_REPLICATION=On -DUSE_ENTERPRISE=On ..
+
+make -j$concurrency
+
+# copy binaries to preserve them
+cd ..
+cp -a build-jenkins/bin/* build/bin
+cp -a build-jenkins/etc/* build/etc
+cp -a build-jenkins/tests/* build/tests
