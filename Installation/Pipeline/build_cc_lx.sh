03399c3de750832aceb61912b4e04ec96b2b9317
Frank Celler <frank@arangodb.com>
2017-06-04 00:38:40 +0200

    cleanup

--- Installation/Pipeline/build_cc_lx.sh
+++ Installation/Pipeline/build_cc_lx.sh
@@ -2,17 +2,19 @@
 
 concurrency=$1
 
-mkdir -p build-jenkins
-mkdir -p build/bin
-mkdir -p build/etc
-mkdir -p build/tests
+(
+    mkdir -p build-jenkins
+    cd build-jenkins
 
-cd build-jenkins
-CXXFLAGS=-fno-omit-frame-pointer cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_MAINTAINER_MODE=On -DUSE_CATCH_TESTS=On -DUSE_FAILURE_TESTS=On -DDEBUG_SYNC_REPLICATION=On ..
-make -j$concurrency
+    CXXFLAGS=-fno-omit-frame-pointer cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_MAINTAINER_MODE=On -DUSE_CATCH_TESTS=On -DUSE_FAILURE_TESTS=On -DDEBUG_SYNC_REPLICATION=On ..
+
+    make -j$concurrency
+)
 
 # copy binaries to preserve them
-cd ..
-cp -a build-jenkins/bin/* build/bin
-cp -a build-jenkins/etc/* build/etc
-cp -a build-jenkins/tests/* build/tests
+rm -rf build
+mkdir build
+
+cp -a build-jenkins/bin   build
+cp -a build-jenkins/etc   build
+cp -a build-jenkins/tests build
